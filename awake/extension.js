5b08decb438d4a69c3f9c42aa449273ae2345cad
padparadscho <contact@padparadscho.com>
2026-09-11 23:54:35 -0300

    feat(extension): add project scaffolding

---
+++ awake/extension.js
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2026 Padparadscho <contact@padparadscho.com>
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+
+export default class AwakeExtension extends Extension {
+	enable() {}
+
+	disable() {}
+}
